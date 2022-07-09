402fd67c7430c3c114b1779a1d0548bd75d84105
Gazer Suthy <suthashan1234@gmail.com>
2022-07-09 09:39:09 -0400

    alignment in being

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,6 +1,5 @@
 import React from "react";
 import GazerSuthy from "../images/profile-picture.png";
-import Cause from "../images/hero-cause.jpg";
 
 const Hero = ({ homeSectionRef }) => {
   return (
